722766baa9419c92545fc27aaa7112c1a070e2dd
Dmitry <hulk295@yandex.ru>
2024-08-13 09:55:10 +0300

    закрепление использования блоков try-except-finally

---
+++ module_8_1.py
@@ -0,0 +1,37 @@
+# a=123.456
+# b = 'mystring'
+def add_everything_up(a, b):
+    try:
+        c = (round((a+b), 3))
+        print(c)
+        # print(round((a + b), 3))
+    except TypeError as typerr:
+        # print(f'нельзя складывать данные разных типов: "{a}" и "{b}" -', typerr)
+        c = str(a)+str(b)
+        print(c)
+    finally:
+        print('='*12)
+        # pass
+
+def add_everything_mod(a, b):
+    try:
+        c = (round((a+b), 3))
+        print(c)
+        # print(round((a + b), 3))
+    except TypeError as typerr:
+        print(f'нельзя складывать данные разных типов: "{type(a)}: {a}" и "{type(b)}: {b}" -', typerr)
+        c = str(a)+str(b)
+        return c
+    else:
+        print(f'все прошло штатно, так как переменные {a} и {b} одного типа')
+    finally:
+        print('='*12)
+        # pass
+
+add_everything_up(123.456, 'строка')
+add_everything_up('яблоко', 4215)
+add_everything_up(123.456, 7)
+print('\n{txt:*^50}'.format(txt=' Теперь c пояcнением ошибок '))
+add_everything_mod(123.456, 'строка')
+add_everything_mod('яблоко', 4215)
+add_everything_mod(123.456, 7)
